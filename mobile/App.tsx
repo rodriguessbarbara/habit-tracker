2213ee2068112883b0ce7e082f9269ac59e14fd1
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2023-01-18 16:52:02 -0300

    feat: :tada: config iniciais mobile
    inicializado com react nativo e instalado dependencia expo

---
+++ mobile/App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold});
+
+  if (!fontsLoaded) return;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Oiiiiiie!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'purple',
+    fontSize: 28,
+  }
+});
