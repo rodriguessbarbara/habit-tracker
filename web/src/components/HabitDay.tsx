dccdc731547eef90d4b5143b56789a9e85820930
Barbara Rodrigues <rodriguessbarbara@hotmail.com>
2023-01-24 15:00:26 -0300

    feat(web): :sparkles: checkbox de habitos e dias/semana

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,6 +1,8 @@
 import * as Popover from "@radix-ui/react-popover";
 import clsx from "clsx";
 import { ProgressBar } from "./ProgressBar";
+import * as Checkbox from "@radix-ui/react-checkbox";
+import { Check } from "phosphor-react";
 
 interface HabitDayProps {
   completed: number;
@@ -14,16 +16,18 @@ export function HabitDay({ completed, amount }: HabitDayProps) {
     <>
       <Popover.Root>
         <Popover.Trigger
-          className={clsx(
-            "w-10 h-10 border-2 rounded-lg",{
-              "bg-zinc-900 border-zinc-800": completedPercentage == 0,
-              "bg-violet-500 border-violet-500": completedPercentage >= 80,
-              "bg-violet-600 border-violet-500": completedPercentage >= 60 && completedPercentage < 80,
-              "bg-violet-700 border-violet-600": completedPercentage >= 40 && completedPercentage < 60,
-              "bg-violet-800 border-violet-700": completedPercentage >= 20 && completedPercentage < 40,
-              "bg-violet-900 border-violet-700": completedPercentage > 0 && completedPercentage < 20,              
-          }
-          )}
+          className={clsx("w-10 h-10 border-2 rounded-lg", {
+            "bg-zinc-900 border-zinc-800": completedPercentage == 0,
+            "bg-violet-500 border-violet-500": completedPercentage >= 80,
+            "bg-violet-600 border-violet-500":
+              completedPercentage >= 60 && completedPercentage < 80,
+            "bg-violet-700 border-violet-600":
+              completedPercentage >= 40 && completedPercentage < 60,
+            "bg-violet-800 border-violet-700":
+              completedPercentage >= 20 && completedPercentage < 40,
+            "bg-violet-900 border-violet-700":
+              completedPercentage > 0 && completedPercentage < 20,
+          })}
         />
 
         <Popover.Portal>
@@ -35,7 +39,36 @@ export function HabitDay({ completed, amount }: HabitDayProps) {
 
             <ProgressBar progress={completedPercentage} />
 
+            <div className="mt-6 flex flex-col gap-3">
+              <Checkbox.Root className="flex items-center gap-3 group">
+                
+                <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                  <Checkbox.Indicator>
+                    <Check size={20} className="text-white" />
+                  </Checkbox.Indicator>
+                </div>
+
+                <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                  Beber 2l água
+                </span>
+              </Checkbox.Root>
+
+              <Checkbox.Root className="flex items-center gap-3 group">
+                
+                <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                  <Checkbox.Indicator>
+                    <Check size={20} className="text-white" />
+                  </Checkbox.Indicator>
+                </div>
+
+                <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                  Ir na academia
+                </span>
+              </Checkbox.Root>
+            </div>
+
             <Popover.Arrow className="fill-zinc-900" height={8} width={16} />
+            
           </Popover.Content>
         </Popover.Portal>
       </Popover.Root>
--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,21 +1,54 @@
 import { Check } from "phosphor-react";
+import * as Checkbox from "@radix-ui/react-checkbox";
+
+const availableWeekDays = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
 
 export function NewHabitForm() {
   return (
     <form className="w-full flex flex-col mt-6">
-      <label htmlFor="title" className="font-semibold leading-tight">Qual seu comprometimento?</label>
+      <label htmlFor="title" className="font-semibold leading-tight">
+        Qual seu comprometimento?
+      </label>
       <input
         type="text"
         id="title"
         placeholder="ex.: Atividade física, horas de sono, etc..."
         autoFocus
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
-
       />
 
-      <label htmlFor="" className="font-semibold leading-tight">Qual a recorrência?</label>
+      <label htmlFor="" className="font-semibold leading-tight mt-6 mb-2">
+        Qual a recorrência?
+      </label>
 
-      <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+      <div className="flex flex-col gap-2 mt-3">
+        {availableWeekDays.map(weekDay => {
+          return (
+            <Checkbox.Root key={weekDay} className="flex items-center gap-3 group">
+              <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
+
+              <span className="text-white leading-tight">{weekDay}</span>
+            </Checkbox.Root>
+          );
+        })}
+      </div>
+
+      <button
+        type="submit"
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500"
+      >
         <Check size={28} weight="bold" />
         Confirmar
       </button>
